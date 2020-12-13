ab131dbe7eb43b8bdae32cc93ee6eef670c5c3a9
raymondddenny <dennyraymondd@gmail.com>
2020-12-13 22:31:39 +0700

    convert html to svelte components

---
+++ src/data/charity.js
@@ -0,0 +1,14 @@
+module.exports={
+    charities:[{
+        id:1,
+        title:'First Charity Projects',
+        category:'Money',
+        thumbnail:'https://images.unsplash.com/photo-1488521787991-ed7bbaae773c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+        pledge:50000,
+        target:1000000,
+        date_end:+new Date('15 December 2020'),
+        profile_photo:'https://images.unsplash.com/photo-1457449940276-e8deed18bfff?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
+        profile_name:'Denny Bueno',
+        no_pledges:0,
+    }]
+}
---
+++ src/pages/Home.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Header from "../components/Header.svelte";
+    import Welcome from "../components/Welcome.svelte";
+    import CharityLists from "../components/CharityLists.svelte";
+    import Promo from "../components/Promo.svelte";
+    import Footer from "../components/Footer.svelte";
+
+    // dummy data
+    import { charities } from "../data/charity";
+</script>
+
+<Header />
+<Welcome />
+<CharityLists {charities} />
+<Promo />
+<Footer />
