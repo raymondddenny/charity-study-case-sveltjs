fa6d5e7cb6ecf5611c7ad86b4778b13b4bd2dd54
raymondddenny <dennyraymondd@gmail.com>
2020-12-14 12:00:45 +0700

    add SPA route using page.js, add About and Contact page

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,21 @@
 <script>
+	import router from "page";
 	import Home from "./pages/Home.svelte";
+	import About from "./pages/About.svelte";
+	import Contact from "./pages/Contact.svelte";
+
+	// page akan berubah sesuai page yang diakses
+	let page;
+
+	// router is for setting SPA
+	router("/", () => (page = Home));
+	router("/about", () => (page = About));
+	router("/contact", () => (page = Contact));
+
+	// start router
+	router.start();
 </script>
 
-<Home />
+<!-- set svelte to render component -->
+
+<svelte:component this={page} />
