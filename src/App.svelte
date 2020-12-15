cb7ea1a5b3949aa4b01a1c57eff90b261233e3c1
raymondddenny <dennyraymondd@gmail.com>
2020-12-15 17:12:05 +0700

    add page.js SPA feature

--- src/App.svelte
+++ src/App.svelte
@@ -3,14 +3,27 @@
 	import Home from "./pages/Home.svelte";
 	import About from "./pages/About.svelte";
 	import Contact from "./pages/Contact.svelte";
+	import Donation from "./pages/Donation.svelte";
+	import NotFound from "./pages/NotFound.svelte";
 
 	// page akan berubah sesuai page yang diakses
-	let page;
+	let page, params;
 
 	// router is for setting SPA
 	router("/", () => (page = Home));
 	router("/about", () => (page = About));
 	router("/contact", () => (page = Contact));
+	router(
+		"/donation/:id",
+		(ctx, next) => {
+			params = ctx.params;
+			// next to go to page
+			next();
+		},
+		() => (page = Donation)
+	);
+	// notFound page harus ditaroh paling bawah
+	router("/*", () => (page = NotFound));
 
 	// start router
 	router.start();
@@ -18,4 +31,4 @@
 
 <!-- set svelte to render component -->
 
-<svelte:component this={page} />
+<svelte:component this={page} {params} />
